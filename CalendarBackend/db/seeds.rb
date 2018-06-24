3fc9a2f93483c67e98522fd2163536dac977b302
Martin Sosa <mr.martinsosa@gmail.com>
2018-06-24 18:35:42 -0400

    Scaffolded, modeled and seeded events

--- CalendarBackend/db/seeds.rb
+++ CalendarBackend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(
+    start: DateTime.new(2018, 7, 4, 8),
+    end: DateTime.new(2018,7,4,23),
+    description: "Fourth of July" 
+)
+
